44768ddf14d357e2b177128fe0ffe49563d758b9
Ahmad Musa <ahmad@cloudify.co>
2020-04-09 15:12:20 +0300

    CY-2608-Upgrade-Wagon-On-PY27

--- centos_6/cloudify-wagon-builder.sh
+++ centos_6/cloudify-wagon-builder.sh
@@ -3,6 +3,7 @@ set -e
 echo "Starting..."
 
 CONSTRAINTS_FILE=/packaging/constraints.txt
+REQUIRMENTS_FILE=/packaging/dev-requirements.txt
 
 if test -f /packaging/extra-packaging-instructions.sh
 then
@@ -13,10 +14,15 @@ echo "manylinux1_compatible = False" > "/env/bin/_manylinux.py"
 if test -f ${CONSTRAINTS_FILE}
 then
     echo "## $CONSTRAINTS_FILE exist"
-    wagon create -s . -r -v -f -a '--no-cache-dir -c '${CONSTRAINTS_FILE}''
+    wagon create -r ${REQUIRMENTS_FILE} -v -f -a '--no-cache-dir -c '${CONSTRAINTS_FILE}'' .
 else
     echo "## $CONSTRAINTS_FILE doesn't exist"
-    wagon create -s . -r -v -f
+    wagon create -r ${REQUIRMENTS_FILE} -v -f .
 fi
+
+echo "appending platform to wagon file name"
+OS_DIST=$(python -c 'import platform; print(platform.linux_distribution(full_distribution_name=False)[0])')
+OS_RELEASE=$(python -c 'import platform; print(platform.linux_distribution(full_distribution_name=False)[2])')
+ls /packaging/*.wgn | sed 'p;s/\.wgn/\-'${OS_DIST}'-'${OS_RELEASE}'.wgn/' | xargs -n2 mv
 
 cp -R * /workspace/build/
