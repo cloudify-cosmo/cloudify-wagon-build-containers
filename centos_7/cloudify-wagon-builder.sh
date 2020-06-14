ac4bccd87c7612e5937bc53b0dcd948c6eb15fcf
AdarShaked <adar@cloudify.co>
2020-06-14 18:25:32 +0300

    revert local changes

--- centos_7/cloudify-wagon-builder.sh
+++ centos_7/cloudify-wagon-builder.sh
@@ -4,13 +4,6 @@ echo "Starting..."
 
 CONSTRAINTS_FILE=/packaging/constraints.txt
 
-REQUIRMENTS_FILE=/packaging/dev-requirements.txt
-
-if ! test -f ${REQUIRMENTS_FILE}
-then
-    REQUIRMENTS_FILE=/packaging/requirements.txt
-fi
-
 if test -f /packaging/extra-packaging-instructions.sh
 then
     source /packaging/extra-packaging-instructions.sh
@@ -20,10 +13,10 @@ echo "manylinux1_compatible = False" > "/env/bin/_manylinux.py"
 if test -f ${CONSTRAINTS_FILE}
 then
     echo "## $CONSTRAINTS_FILE exist"
-    wagon create -r ${REQUIRMENTS_FILE} -v -f -a '--no-cache-dir -c '${CONSTRAINTS_FILE}'' .
+    wagon create -s . -r -v -f -a '--no-cache-dir -c '${CONSTRAINTS_FILE}''
 else
     echo "## $CONSTRAINTS_FILE doesn't exist"
-    wagon create -r ${REQUIRMENTS_FILE} -v -f .
+    wagon create -s . -r -v -f
 fi
 
 cp -R * /workspace/build/
