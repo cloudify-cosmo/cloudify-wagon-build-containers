66f086e9fb15b248d92782fbd0b5e4cc1e06f028
AdarShaked <adar@cloudify.co>
2020-06-15 19:19:48 +0300

    code review fixes

--- centos_7_py2py3/cloudify-wagon-builder.sh
+++ centos_7_py2py3/cloudify-wagon-builder.sh
@@ -3,21 +3,20 @@ set -e
 echo "Starting..."
 
 CONSTRAINTS_FILE=/packaging/constraints.txt
-REQUIRMENTS_FILE=/packaging/dev-requirements.txt
+REQUIREMENTS_FILE=/packaging/dev-requirements.txt
 
-if test -f /packaging/extra-packaging-instructions.sh
-then
+if [[ -f /packaging/extra-packaging-instructions.sh ]]; then
+
     source /packaging/extra-packaging-instructions.sh
 fi
 source /$1/bin/activate
 echo "manylinux1_compatible = False" > "/$1/bin/_manylinux.py"
-if test -f ${CONSTRAINTS_FILE}
-then
+if [[ -f "${CONSTRAINTS_FILE}" ]]; then
     echo "## $CONSTRAINTS_FILE exist"
-    wagon create --pip /$1/bin/pip --pip /$2/bin/pip --pyver "27" --pyver "36" -r ${REQUIRMENTS_FILE} -v -f -a '--no-cache-dir -c '${CONSTRAINTS_FILE}'' .
+    wagon create --pip /$1/bin/pip --pip /$2/bin/pip --pyver "27" --pyver "36" -r ${REQUIREMENTS_FILE} -v -f -a '--no-cache-dir -c '${CONSTRAINTS_FILE}'' .
 else
     echo "## $CONSTRAINTS_FILE doesn't exist"
-    wagon create --pip /$1/bin/pip --pip /$2/bin/pip --pyver "27" --pyver "36" -r ${REQUIRMENTS_FILE} -v -f .
+    wagon create --pip /$1/bin/pip --pip /$2/bin/pip --pyver "27" --pyver "36" -r ${REQUIREMENTS_FILE} -v -f .
 fi
 
 cp -R * /workspace/build/
