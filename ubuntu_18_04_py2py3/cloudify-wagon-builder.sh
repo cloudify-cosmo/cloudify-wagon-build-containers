82ef7b1806fe4aa7bce5099427d648410eed24b1
AdarShaked <adar@cloudify.co>
2020-07-15 08:44:11 +0300

    added first version of py2py3 wagon for 18.04

---
+++ ubuntu_18_04_py2py3/cloudify-wagon-builder.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+echo "Starting..."
+
+CONSTRAINTS_FILE=/packaging/constraints.txt
+REQUIREMENTS_FILE=/packaging/dev-requirements.txt
+
+if [[ -f /packaging/extra-packaging-instructions.sh ]]; then
+
+    source /packaging/extra-packaging-instructions.sh
+fi
+source /$1/bin/activate
+echo "manylinux1_compatible = False" > "/$1/bin/_manylinux.py"
+if [[ -f "${CONSTRAINTS_FILE}" ]]; then
+    echo "## $CONSTRAINTS_FILE exist"
+    wagon create --pip /$1/bin/pip --pip /$2/bin/pip --pyver "27" --pyver "36" --build-tag "Ubuntu-bionic" -r ${REQUIREMENTS_FILE} -v -f -a '--no-cache-dir -c '${CONSTRAINTS_FILE}'' .
+else
+    echo "## $CONSTRAINTS_FILE doesn't exist"
+    wagon create --pip /$1/bin/pip --pip /$2/bin/pip --pyver "27" --pyver "36" --build-tag "Ubuntu-bionic" -r ${REQUIREMENTS_FILE} -v -f .
+fi
+
+cp -R * /workspace/build/
