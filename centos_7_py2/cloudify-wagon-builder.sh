ce1f77673b7b743c96f5844860502cd06eb6e2a8
AdarShaked <adar@cloudify.co>
2020-10-25 22:27:31 +0200

    bump wagon to 0.11.0 for py2 wagons

--- centos_7_py2/cloudify-wagon-builder.sh
+++ centos_7_py2/cloudify-wagon-builder.sh
@@ -3,6 +3,7 @@ set -e
 echo "Starting..."
 
 CONSTRAINTS_FILE=/packaging/constraints.txt
+REQUIRMENTS_FILE=/packaging/dev-requirements.txt
 
 if test -f /packaging/extra-packaging-instructions.sh
 then
@@ -13,10 +14,10 @@ echo "manylinux1_compatible = False" > "/env/bin/_manylinux.py"
 if test -f ${CONSTRAINTS_FILE}
 then
     echo "## $CONSTRAINTS_FILE exist"
-    wagon create -s . -r -v -f -a '--no-cache-dir -c '${CONSTRAINTS_FILE}''
+    wagon create -r ${REQUIRMENTS_FILE} -v -f -a '--no-cache-dir -c '${CONSTRAINTS_FILE}'' .
 else
     echo "## $CONSTRAINTS_FILE doesn't exist"
-    wagon create -s . -r -v -f
+    wagon create -r ${REQUIRMENTS_FILE} -v -f .
 fi
 
 cp -R * /workspace/build/
