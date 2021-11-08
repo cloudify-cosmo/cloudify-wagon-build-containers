55e631b577ba704a0966de5a04d59850d06976f2
EarthmanT <EarthmanT@users.noreply.github.com>
2021-11-08 11:22:08 -0500

    Update cloudify-wagon-builder.sh (#29)

--- aarch_64/centos_7_py3/cloudify-wagon-builder.sh
+++ aarch_64/centos_7_py3/cloudify-wagon-builder.sh
@@ -14,10 +14,10 @@ echo "manylinux1_compatible = False" > "/env/bin/_manylinux.py"
 if test -f ${CONSTRAINTS_FILE}
 then
     echo "## $CONSTRAINTS_FILE exist"
-    wagon create -r ${REQUIRMENTS_FILE} --build-tag "centos-Core" -v -f -a '--no-cache-dir -c '${CONSTRAINTS_FILE}'' .
+    wagon create -r ${REQUIRMENTS_FILE} --build-tag "aarch-64" -v -f -a '--no-cache-dir -c '${CONSTRAINTS_FILE}'' .
 else
     echo "## $CONSTRAINTS_FILE doesn't exist"
-    wagon create -r ${REQUIRMENTS_FILE} --build-tag "centos-Core" -v -f .
+    wagon create -r ${REQUIRMENTS_FILE} --build-tag "aarch-64" -v -f .
 fi
 
 cp -R * /workspace/build/
