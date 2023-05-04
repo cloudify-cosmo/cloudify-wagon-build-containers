0b0da600a7a959e7784dd3c0a4d6ee862e513b63
EarthmanT <EarthmanT@users.noreply.github.com>
2023-05-04 16:25:55 -0400

    RND-483 Support Python 3.11 for RH8 RH7 Centos7 and Centos7 aarch (#51)

---
+++ centos_7_py311/cloudify-wagon-builder.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+echo "Starting..."
+
+alias python=/usr/local/bin/python3.11
+
+CONSTRAINTS_FILE=/packaging/constraints.txt
+REQUIRMENTS_FILE=/packaging/dev-requirements.txt
+
+if test -f /packaging/extra-packaging-instructions.sh
+then
+    source /packaging/extra-packaging-instructions.sh
+fi
+source /env/bin/activate
+echo "manylinux1_compatible = False" > "/env/bin/_manylinux.py"
+if test -f ${CONSTRAINTS_FILE}
+then
+    echo "## $CONSTRAINTS_FILE exist"
+    wagon create -r ${REQUIRMENTS_FILE} --build-tag "el" -v -f -a '--no-cache-dir -c '${CONSTRAINTS_FILE}'' .
+else
+    echo "## $CONSTRAINTS_FILE doesn't exist"
+    wagon create -r ${REQUIRMENTS_FILE} --build-tag "el" -v -f .
+fi
+
+cp -R * /workspace/build/
